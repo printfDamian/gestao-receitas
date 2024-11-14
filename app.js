5db120dab31323053bb03e03c5eec1a8823ad005
SamuelSantos <samueldamiaosantos@gmail.com>
2024-11-14 14:45:15 +0000

    successfull conection with the database

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ const path = require("path");
 const app = express();
 const fs = require('fs');
 require('dotenv').config();
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: 'localhost',
+  dialect: 'mysql',
+  dialectModule: require('mysql2')
+});
+
 let contador = [];
 
 // App Config
@@ -15,6 +23,21 @@ app.use(express.static(path.join(__dirname, "./public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Test database connection
+sequelize.authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+
+// Synchronize models
+sequelize.sync()
+  .then(() => {
+    console.log('Database & tables created!');
+  });
+
 // Routes / Endpoints
 const index = require('./routes/index');
 app.use(index);    
@@ -24,13 +47,6 @@ app.use(routes);
 
 const register = require('./routes/register');
 app.use(register);
-
-const login = require('./routes/login');
-app.use(login);
-
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/views/erro.html"));
-});
 
 console.log("Server running on http://localhost:" + port);
 app.listen(port);
--- routes/routes.js
+++ routes/routes.js
@@ -2,9 +2,26 @@ var express = require("express");
 var router = express.Router();
 var path = require("path");
 const fs = require("fs");
-const userController = require('../controllers/userController'); 
+const ejs = require("ejs");
+const pathToTemplate = path.join(__dirname, "../templates/htmlTemplate.html");
 
-router.post('/register', userController.register);
-router.post('/login', userController.login);
-
+router.get('*', (req, res) => {
+    fs.readFile(__dirname + "/../public/views/erro.html", "utf8", (err, data) => {
+        if (err) res.status(500).send(err.message);
+        
+        ejs.renderFile(pathToTemplate, {
+            docTitle: "GR - Register",
+            upperNavBar: true,
+            content: data,
+            footer: true
+        },
+        (err, str) => {
+            if (err) {
+                res.status(500).send(err.message);
+            } else {
+                res.status(200).send(str);
+            }
+        });
+    });
+});
 module.exports = router;
