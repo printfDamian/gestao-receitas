9a63683724f241360232ff8ec930210c727502a6
andre-jps <202300830@estudantes.ips.pt>
2024-11-18 09:37:38 +0000

    Update README and app.js to include database sync option

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const fs = require('fs');
-require('dotenv').config();
-const { Sequelize } = require('sequelize');
-const sequelizeInstance = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: 'localhost',
-  dialect: 'mysql',
-  dialectModule: require('mysql2')
-});
+const fs = require("fs");
+require("dotenv").config();
+const { Sequelize } = require("sequelize");
+const sequelizeInstance = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: "localhost",
+        dialect: "mysql",
+        dialectModule: require("mysql2"),
+    }
+);
 
 let contador = [];
 // App Config
@@ -22,22 +27,24 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Synchronize models
-sequelizeInstance.sync()
-  .then(() => {
-    console.log('Models synced');
-  });
+if (process.argv.includes("--sync")) {
+	sequelizeInstance.sync({ force: true }).then(() => {
+		console.log("Models synced");
+	});
+}
 
 // Routes / Endpoints
-const index = require('./routes/index');
-app.use(index);    
+const index = require("./routes/index");
+app.use(index);
 
-const register = require('./routes/register');
+const register = require("./routes/register");
 app.use(register);
 
-const login = require('./routes/login');
+const login = require("./routes/login");
 app.use(login);
 
-const routes = require('./routes/routes');
+const routes = require("./routes/routes");
 app.use(routes);
-console.log("Server running on http://localhost:" + port);
+
 app.listen(port);
+console.log("Server running on http://localhost:" + port);
